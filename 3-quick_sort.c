8e6255de729abf9ffdaf85fd2ac9b7b7c81b5486
Ahmed Mohamed <ahmed.mohamed91196@gmail.com>
2024-03-05 18:56:02 -0800

    fixed array's printing

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -8,12 +8,21 @@
 
 void quick_sort(int *array, size_t size)
 {
+	static int *original_array;
+	static size_t original_size;
 	int pivot, j, i = 0;
 
 	pivot = size - 1;
+	original_array = NULL;
+	original_size = 0;
 
 	if (size <= 1)
 		return;
+	if (original_array == NULL)
+	{
+		original_array = array;
+		original_size = size;
+	}
 	for (j = 0, i = j - 1; j < pivot; j++)
 	{
 		if (array[j] < array[pivot])
@@ -24,7 +33,7 @@ void quick_sort(int *array, size_t size)
 	}
 	i++;
 	swap_int(&array[i], &array[pivot]);
-
+	print_array(original_array, original_size);
 	quick_sort(array, i);
 	quick_sort(array + i + 1, size - i - 1);
 }
